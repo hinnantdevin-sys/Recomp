9de3546dca3ceaf1916dd1e5b30e7b7a77a6005e
hinnantdevin-sys <hinnantdevin@gmail.com>
2026-05-17 14:56:19 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: ['es2020', 'safari15'],
+    rollupOptions: {
+      output: {
+        // Single chunk to avoid cross-origin module issues
+        manualChunks: undefined,
+      }
+    }
+  },
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Resource-Policy': 'same-origin',
+    }
+  }
 })
